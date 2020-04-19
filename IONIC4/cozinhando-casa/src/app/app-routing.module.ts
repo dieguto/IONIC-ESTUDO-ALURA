361fe5dab10e5b969014dad2cdbcf7281e3217aa
Diego Augusto <diegoaugusto1606@gmail.com>
2020-04-19 13:58:30 -0300

    Quarta aula de ionic

---
+++ IONIC4/cozinhando-casa/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
+  },
+  {
+    path: "detalhes-receita",
+    loadChildren: () =>
+      import("./receita-detalhes/receita-detalhes.module").then(
+        (m) => m.ReceitaDetalhesPageModule
+      ),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
